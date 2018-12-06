80a861c3d95141c91765bfa3bef360c727b91d6b
Uranel <pharanula@gmail.com>
2018-12-06 15:31:14 +0900

    mongodb 연결

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var createError = require('http-errors');
 var express = require('express');
-var favicon = require('serve-favicon');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var sassMiddleware = require('node-sass-middleware');
 var session = require('express-session');
 var methodOverride = require('method-override');
 var flash = require('connect-flash');
 var mongoose   = require('mongoose');
 
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+var questions = require('./routes/questions');
 
 var app = express();
 
@@ -33,16 +33,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
 
 //favicon
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -51,6 +46,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// _method를 통해서 method를 변경할 수 있도록 함. PUT이나 DELETE를 사용할 수 있도록.
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
+
+// sass, scss를 사용할 수 있도록
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false, // true = .sass and false = .scss
+  debug: true,
+  sourceMap: true
+}));
+
+// session을 사용할 수 있도록.
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
+}));
+
+app.use(flash()); // flash message를 사용할 수 있도록
+
+// pug의 local에 현재 사용자 정보와 flash 메시지를 전달하자.
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.session.user;
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
 // Route
 app.use('/', index);
 app.use('/users', users);
@@ -58,7 +81,9 @@ app.use('/questions', questions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
@@ -71,5 +96,16 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+//=======================================================
+// mongodb connect
+//=======================================================
+mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
+const connStr = 'mongodb://bsh:bsh@https://sheltered-chamber-43409.herokuapp.com/Webproject';
+// 아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
+// const connStr = 'mongodb://dbuser1:mju12345@ds113825.mlab.com:13825/sampledb1';
+mongoose.connect(connStr, {useMongoClient: true });
+mongoose.connection.on('error', console.error);
 
 module.exports = app;
