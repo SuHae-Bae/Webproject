6d4b018000a588baa820e4e87c52a7833ce1859a
Uranel <pharanula@gmail.com>
2018-12-06 15:54:27 +0900

    mongodb연결, favicon 수정

--- app.js
+++ app.js
@@ -102,10 +102,14 @@ app.use(function(err, req, res, next) {
 // mongodb connect
 //=======================================================
 mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
-const connStr = 'mongodb://bsh:bsh@https://sheltered-chamber-43409.herokuapp.com/Webproject';
+const connStr = 'mongodb://bsh:bsh@cluster0-shard-00-00-gvboc.mongodb.net:27017,cluster0-shard-00-01-gvboc.mongodb.net:27017,cluster0-shard-00-02-gvboc.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 // 아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
 // const connStr = 'mongodb://dbuser1:mju12345@ds113825.mlab.com:13825/sampledb1';
-mongoose.connect(connStr, {useMongoClient: true });
-mongoose.connection.on('error', console.error);
+mongoose.connect(connStr, {useMongoClient: true })
+.then(
+  () => {console.log('connected');}, 
+  (err) => { console.error(err);}
+);
+// mongoose.connection.on('error', console.error);
 
 module.exports = app;
